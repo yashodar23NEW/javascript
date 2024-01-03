54c7f6088bc7e3c7c27025cc9946ef9adc2859bc
Ruwanthi Yashodara <119606787+yashodar23NEW@users.noreply.github.com>
2024-01-03 09:42:26 +0530

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,89 @@
+'some'+'text'
+'sometext'
+'some'+'more'+'text'
+'somemoretext'
+typeof 2
+'number'
+typeof 'geer'
+'string'
+'hello'+ 6
+'hello6'
+typeof 'heee4'
+'string'
+
+'$ '+(20.95+7.99)
+'$ 28.939999999999998'
+'$'+(2095+799)/100
+'$28.94'
+
+
+'Items ('+(1+1)+'):$'+(2095+799)/100
+'Items (2):$28.94'
+'Items('+(1+1)+'):$'+(2095+799)/100
+'Items(2):$28.94'
+alert('Items('+(1+1)+'):$'+(2095+799)/100);
+
+
+
+"I'm learning javascript"
+"I'm learning javascript"
+'I\'m learning javascript'
+"I'm learning javascript"
+alert('some\n text');
+undefined
+`hello`
+'hello'
+`items (${1+1}):$${(799+2095)/100}`
+'items (2):$28.94'
+`some
+text`
+'some\ntext'
+'My name is: Ruwanthi'
+'My name is: Ruwanthi'
+'Ruwanthi'
+'Ruwanthi'
+'My name is: ruwanthi'
+'My name is: ruwanthi'
+'My name is:'+'Ruwanthi'
+'My name is:Ruwanthi'
+'$'+(5+3)
+'$8'
+
+
+
+
+'Toatal cost:$'+(5+3)
+'Toatal cost:$8'
+`Total cost:$${5+3}`
+'Total cost:$8'
+alert(`Total cost:$${5+3}`)
+undefined
+(599+295)/100
+8.94
+'Total cost:$'+(599+295)/100
+'Total cost:$8.94'
+`Total cost:$${(599+295)/100}`
+
+'Total cost:$8.94'
+
+
+
+
+
+
+
+alert(`Total cost:$${(599+295)/100}`)
+undefined
+alert(`Total cost:$${(599+295)/100}
+Thank you, come again`)
+undefined
+`Items(${2+2}):$${(2095*2+799*2)/100}`
+'Items(4):$57.88'
+`Shipping & handling:$${(499+499)/100}`
+'Shipping & handling:$9.98'
+`Total before tax:$${(2095*2+799*2+499+499)/100}`
+'Total before tax:$67.86'
+`Estimated tax(10%):$${Math.round((2095*2+799*2+499+499)*0.1)/100}`
+'Estimated tax(10%):$6.79'
+'$'+(2095+799)/100
+'$28.94'
